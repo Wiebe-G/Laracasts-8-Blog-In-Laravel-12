bcd69103bbb8ef3a3d0dd7e652150c23567f1aad
Wiebe Gouma <goumawiebe1234@proton.me>
2026-02-03 14:59:30 +0100

    Blade templates hoofdstuk afgerond

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,21 @@
 	<h1 class="text-center font-bold">Blog </h1>
 	<div class="flex flex-col items-center gap-3">
 
-		<?php foreach($posts as $post) : ?>
-		<article>
-			<h1>
-				<a href="/posts/<?= $post->slug ?>">
-					Titel: <?= $post->title ?>
-				</a>
-			</h1>
-			<p>Datum: <?= $post->date ?></p>
-			{{-- <p>Body: <?= $post->body ?></p> --}}
-			<p>Excerpt: <?= $post->excerpt ?></p>
-		</article>
-		<?php endforeach; ?>
+		@forelse($posts as $post)
+			<article>
+				<h1>
+					<a href="/posts/{{ $post->slug }}">
+						Titel: {{ $post->title }}
+					</a>
+				</h1>
+				<p>Datum: {{ $post->date }}</p>
+				{{-- <p>Body: <?= $post->body ?></p> --}}
+				<p>Excerpt: {{ $post->excerpt }}</p>
+			</article>
+			<div class="mb-5 mt-5 h-px w-full bg-gray-500"></div>
+		@empty
+			<div>Geen posts gevonden</div>
+			< @endforelse
 	</div>
 
 </x-layout>
